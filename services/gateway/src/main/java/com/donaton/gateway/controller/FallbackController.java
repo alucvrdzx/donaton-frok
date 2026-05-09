c547242c7c2b8223a09c2d8fada47f6628add996
Poulette <alia.serrano@duocuc.cl>
2026-05-09 00:16:47 -0400

    Implementacion API Gateway y Circuit Breaker

---
+++ services/gateway/src/main/java/com/donaton/gateway/controller/FallbackController.java
@@ -0,0 +1,26 @@
+package com.donaton.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback/inventario")
+    public String inventarioFallback() {
+
+        return "Servicio de inventario temporalmente no disponible";
+    }
+
+    @GetMapping("/fallback/donaciones")
+    public String donacionesFallback() {
+
+        return "Servicio de donaciones temporalmente no disponible";
+    }
+
+    @GetMapping("/fallback/logistica")
+    public String logisticaFallback() {
+
+        return "Servicio de logística temporalmente no disponible";
+    }
+}
