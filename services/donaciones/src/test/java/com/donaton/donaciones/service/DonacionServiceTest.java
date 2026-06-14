05f48fbfc9ec4b5d70ec953640377bf5c467b2f1
Poulette04 <alia.serrano@duocuc.cl>
2026-06-14 10:20:12 -0400

    arreglo de registro

--- services/donaciones/src/test/java/com/donaton/donaciones/service/DonacionServiceTest.java
+++ services/donaciones/src/test/java/com/donaton/donaciones/service/DonacionServiceTest.java
@@ -1,16 +1,25 @@
 package com.donaton.donaciones.service;
 
-import static org.mockito.Mockito.*;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import com.donaton.donaciones.factory.DonacionFactory;
+import com.donaton.donaciones.model.DonacionDetalle;
 import com.donaton.donaciones.repository.DonacionRepository;
+
+
 
 @ExtendWith(MockitoExtension.class)
 class DonacionServiceTest {
@@ -26,5 +35,52 @@ class DonacionServiceTest {
 
     @InjectMocks
     private DonacionService service;
+
+   @Test
+void debeCrearDonacionNueva() {
+
+    DonacionDetalle donacion = new DonacionDetalle();
+
+    donacion.setNombreDonante("Juan");
+    donacion.setTipoDonacion("ROPA");
+    donacion.setCantidad(10.0);
+    donacion.setDetalle("Polera");
+    donacion.setUnidadMedida("unidades");
+
+    when(repository.findByNombreDonanteAndTipoDonacionAndDetalle(
+            "Juan",
+            "ROPA",
+            "Polera"))
+        .thenReturn(Optional.empty());
+
+    when(factory.crearDonacion(
+            "Juan",
+            "ROPA",
+            10.0,
+            "Polera"))
+        .thenReturn(donacion);
+
+    when(repository.save(donacion))
+        .thenReturn(donacion);
+
+    DonacionDetalle resultado = service.crearDonacion(
+            "Juan",
+            "ROPA",
+            10.0,
+            "Polera");
+
+    assertNotNull(resultado);
+
+    assertEquals("Juan", resultado.getNombreDonante());
+
+    assertEquals(10.0, resultado.getCantidad());
+
+    verify(repository).save(donacion);
+
+    verify(rabbitTemplate).convertAndSend(
+        anyString(),
+        anyString(),
+        any(Object.class));
+}
 
 }
