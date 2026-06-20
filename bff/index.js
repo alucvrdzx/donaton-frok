b126b42808900a613942c36a6c7b687153fc0444
juan villagra <jua.villagra@duocuc.cl>
2026-06-20 02:21:10 -0400

    fix: Add PATCH method and wildcard origin to CORS

--- bff/index.js
+++ bff/index.js
@@ -7,7 +7,11 @@ const { PORT } = require('./config');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
